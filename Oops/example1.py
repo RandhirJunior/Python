c7e4dc957c3d55309a4bc3daf273c69bc0ff4c57
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-05-09 22:30:43 +0530

    Update example1.py

--- Oops/example1.py
+++ Oops/example1.py
@@ -6,6 +6,15 @@
 
 #Once class is ready===> multiple objects we can create.
 
+'''
+1.self is a reference variable which is always pointing to current object.
+   Within the python class to refer current object we should use self variable.
+2. The first argument to the constructor and instance method should be self.
+3. PVM is responsible to provide value for self argument and we are not required to provide explicitly.   
+4. By using self we can declare instance variable
+5. By usingself we can access instance variable.
+'''
+
 class Student:
 	def __init__(self,name,rollno):   #This is the constructor
 		self.name=name
@@ -16,6 +25,6 @@ class Student:
 s=Student('Randhir',100)  #Object Creation step and s is the reference of Student object
 print(s.name)
 print(s.rollno)
-s.talk()		
+s.talk()	# Python virtual machine(PVM) is responsible to provide self.	
 s1=Student('Sunny',200)
 s1.talk()
