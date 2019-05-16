1aff1a6311096557a597f4f9e21e41080d3e9a3e
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-05-16 19:03:22 +0530

    Create Instance_Variables.py

---
+++ Oops/Instance_Variables.py
@@ -0,0 +1,14 @@
+#------Instance variables-----------------------------
+#If the value of a variable is varied from object to object such types of variable is called instance variables.
+# For every object a seperate copy of instance variable is created.
+
+class Student:
+    def __init__(self,name,roll):
+        self.name=name
+        self.roll=roll
+        
+s1=Student('randhir',100)
+s2=Student('ravi',300)
+s3=Student('rama',600)
+
+# Where we have to declare instance variables............
