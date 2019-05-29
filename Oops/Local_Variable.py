599dd7a8c65a0e21c65fb8794efc94b60860f88e
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-05-29 22:25:17 +0530

    Update Local_Variable.py

--- Oops/Local_Variable.py
+++ Oops/Local_Variable.py
@@ -13,5 +13,28 @@ t=Test()
 t.m1()
 t.m2()
 
-#
+#Example 2:
+
+class Test:
+    def avgerage(self,list):
+        result=sum(list)/len(list)
+        print(result)
+        
+        
+t=Test()
+t.avgerage([10,20,30,40])
+
+#---------------------Global Variable---------------------------------------------------------
+
+x=100
+class Test:
+    def m1(self):
+        print(x)
+    def m2(self):
+        print(x)
+t=Test()
+t.m1()
+t.m2()
+
+
 
