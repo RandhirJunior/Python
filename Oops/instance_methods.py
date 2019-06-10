95c0f4c87aa8a5334daa00a65e268c56d2a9cb96
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-06-10 22:15:25 +0530

    Update instance_methods.py

--- Oops/instance_methods.py
+++ Oops/instance_methods.py
@@ -1,4 +1,5 @@
 #instance methods
+#If we are using atleast one instance variable ---->instance methods.
 
 class Student:
     def __init__(self,name,marks):
@@ -27,3 +28,55 @@ for i in range(n):
     s.display() # Calling instance method
     s.grade()  # Calling instance method
     print('*'*20)
+
+# setter and getter methods--------------------------------------------------
+# setter method:- to set the data to the object.
+# getter method:- to get the data from the object.
+
+#setter Syntax:---
+def setVariableName(self,variableName):
+    self.varaibleName=variableName
+    
+def setMarks(self,marks):
+    self.marks=marks
+    
+# getter Syntax:--
+def getMarks(self):
+    #//Validation stuff
+    return self.marks
+    
+#---------------------------------------------------------------------------------------------
+#print(s.name) // direct access no validations
+#print(s.getName()) // Hiding data behind method --->Encapuslation
+#
+#s.name='randhir' // not good programming
+#
+#-----------------Examples-----------------------------------------------------------------------------
+class Student:
+   def setName(self,name):
+    self.name=name
+    
+   def getName(self):
+    return self.name
+   
+   def setMarks(self,marks):
+    self.marks=marks
+    
+   def getMarks(self):
+    return self.marks
+
+n=int(input('Enter Number of Students:'))
+for i in range(n):
+    name=input('Enter Name:')
+    marks=int(input('Enter Marks:'))
+    s=Student()
+    s.setName(name)
+    s.setMarks(marks)
+    
+    print('Hi',s.getName())
+    print('Your Marks are',s.getMarks())
+    print('*'*20)
+#
+    
+    
+   
