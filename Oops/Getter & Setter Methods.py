93671e2ddf84f5e2cf6a6bb4934a170cf36d9fc9
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-06-28 16:42:22 +0530

    Update Getter & Setter Methods.py

--- Oops/Getter & Setter Methods.py
+++ Oops/Getter & Setter Methods.py
@@ -1,1 +1,39 @@
+# Setter method Syntax
+# def setVariablename(self,variableName):
+#    self.variableName=variableName
+#
+#  Example:-
+#  def setMarks(self,Marks):
+#      self.Marks=Marks
+#
+#   def getMarks(self):
+#       return self.Marks
 
+# Examples 1:
+
+class Student:
+   def setName(self,name):
+    self.name=name
+    
+   def getName(self):
+    return self.name
+   
+   def setMarks(self,marks):
+    self.marks=marks
+    
+   def getMarks(self):
+    return self.marks
+
+n=int(input('Enter Number of Students:'))
+for i in range(n):
+    name=input('Enter Name:')
+    marks=int(input('Enter Marks:'))
+    s=Student()
+    s.setName(name)
+    s.setMarks(marks)
+    
+    print('Hi',s.getName())
+    print('Your Marks are',s.getMarks())
+    print('*'*20)
+    
+  
