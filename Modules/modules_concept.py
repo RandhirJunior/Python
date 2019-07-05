bc211420795907bab262618d489120a687220382
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2019-07-05 16:43:39 +0530

    Create modules_concept.py

---
+++ Modules/modules_concept.py
@@ -0,0 +1,14 @@
+# A group of functions and variables saved to a file is considered as module.
+# The main advantage of module is reusesablity,readablity and mainting is good.
+#
+# Syntax:-
+# import module1 as m1
+#
+# Reloading a module:
+#----------------------------------------------------------------------------------------
+# For every module .pyc file is generated and it saved under folder __pycache__ in cwd.
+# Reason of Reloading of a module:- When we are chnaging code level in any module ,then we need to import is again by Reloading of a module by below process.
+#
+# Example:-
+imp module
+relaod() function
