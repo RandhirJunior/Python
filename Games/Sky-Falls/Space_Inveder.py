07cc38a0e672f3f84450e689d95be5e05f816003
RandhirJunior <48823300+RandhirJunior@users.noreply.github.com>
2020-04-26 22:30:08 +0530

    Add files via upload

---
+++ Games/Sky-Falls/Space_Inveder.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 13:06:41 2020
+
+@author: Randhirs
+"""
+
+import pygame
+import random
+import math
+
+from pygame import mixer
+# Initalize the pygame
+pygame.init()
+
+#RED COLOR
+BACKGROUND_COLOR = (0, 0, 0)
+
+# Screen Dimensiin
+WIDTH = 800
+HEIGHT = 600
+
+# create the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# BAckgroung
+background = pygame.image.load('background.jpg')
+
+# Backgroung Sound
+mixer.music.load('background.wav')
+mixer.music.play(-1)
+
+# Title and Icon
+pygame.display.set_caption('Space Craft')
+icon = pygame.image.load('player.png')
+pygame.display.set_icon(icon)
+
+# Players
+playersImg = pygame.image.load('aircraft.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy list
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+number_of_enemy = 7
+
+# Enemy
+for i in range(number_of_enemy) :
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 753))
+    enemyY.append(random.randint(50, 150))
+    # X and Y asix movement
+    enemyX_change.append(5)
+    enemyY_change.append(40)
+
+
+# Bullet
+# Ready :- You can't see the bullet on the screen
+# Fire :- The bullet is currently moving
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+# X and Y asix movement
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+# Score
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+
+dev_font = pygame.font.Font('freesansbold.ttf', 10)
+dev_X =650
+dev_Y =580
+
+# Game Over Text
+
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def show_score(x, y) :
+    score = font.render("Score:" + str(score_value) , True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def developed_by(x, y) :
+    dev_text = dev_font.render("Developed by Randhir Gupta" , True, (189, 253, 61))
+    screen.blit(dev_text, (x, y))
+    
+def game_over_text() :
+    over_text = over_font.render("GAME OVER !!", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+def player(x, y) :
+    screen.blit(playersImg, (x, y))
+    
+
+def enemy(x, y, i) :
+    screen.blit(enemyImg[i], (x, y)) 
+    
+    
+def fire_bullet(x, y) :
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+    
+def isCollision(enemyX, enemyY, bulletX, bulletY) :
+    # Finding distance bettween two coordinates
+    distance = math.sqrt((math.pow(enemyX-bulletX,2)) + (math.pow(enemyY - bulletY,2)))
+    if distance < 27 :
+        return True
+    else :
+        return False
+
+
+# Game Loop, It will run infinite until window is not closed
+running = True
+while running:
+    
+    # RGB- RED, GREEN ,BLUE
+    screen .fill(BACKGROUND_COLOR)
+    #Background image
+    screen.blit(background, (0, 0,))
+
+    
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            running = False
+            
+        # If keystroke is presssed check wether it's right or left.
+        if event.type == pygame.KEYDOWN :
+           
+            if event.key == pygame.K_LEFT :
+                playerX_change = -7
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 7
+            if event.key == pygame.K_SPACE :
+                if bullet_state is "ready" :
+                    bullet_Sound = mixer.Sound('laser.wav')
+                    bullet_Sound.play()
+                    bulletX = playerX
+                    fire_bullet(bulletX , bulletY)
+        if event.type == pygame.KEYUP :
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT :
+                playerX_change = 0
+                
+    # Checking boundaries of space ship, So it doesn't go out og frame            
+    playerX += playerX_change
+    # To strick  space ship on window , for left side we need to restrick it at 0 on x-axis and on right side we nned to restrick it on 736 . because image size is 64 pixel and x-axis is 800 pixel
+    if playerX <=0 :
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+        
+        
+    # Enemy movements
+    for i in range(number_of_enemy) :
+        
+        # Game Over
+        if enemyY[i] > 440 :
+            for j in range(number_of_enemy) :
+                enemyY[j] = 2000
+            
+            game_over_text()
+            break
+        
+        enemyX[i] += enemyX_change[i]
+        
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 5
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736 :
+            enemyX_change[i] = -5
+            enemyY[i] += enemyY_change[i]
+            
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision :
+            explosion_Sound = mixer.Sound('explosion.wav')
+            explosion_Sound.play()
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            
+            enemyX[i] = random.randint(0, 753)
+            enemyY[i] = random.randint(50, 150)
+            
+        enemy(enemyX[i], enemyY[i], i)
+           
+     # Bullent Movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+        
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+        
+             
+    player(playerX, playerY)
+    show_score(textX, textY)
+    developed_by(dev_X, dev_Y)
+    pygame.display.update()
